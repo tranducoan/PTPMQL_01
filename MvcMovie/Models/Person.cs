e4f4e3474e912b63d239dd7ec55cf278080ca78a
tranducoan <sonsuke655@gmail.com>
2024-03-07 18:24:02 +0700

    7_MVC 20

---
+++ MvcMovie/Models/Person.cs
@@ -0,0 +1,12 @@
+namespace MvcMovie.Models.
+{
+
+    public class Person
+    {
+         public string PersonId { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+//TRAN DUC TOAN  - 2021050642     
+
+    }
+}
--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaaf658abf52e2bdb7d687873609db2edde261a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d2e0104a31a9c93c3e8531b67418ed01da36544")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
