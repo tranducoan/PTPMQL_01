5d2e0104a31a9c93c3e8531b67418ed01da36544
tranducoan <sonsuke655@gmail.com>
2024-03-07 18:18:50 +0700

    7_MVC 20

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4769a1a730e07e84050158d803c9cceecef71db5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaaf658abf52e2bdb7d687873609db2edde261a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
