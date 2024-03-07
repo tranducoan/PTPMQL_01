d2d2422bb806e93f21beac101c0cae215b448be8
tranducoan <sonsuke655@gmail.com>
2024-03-07 17:46:12 +0700

    tao model

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,22 @@
+using System.CodeDom.Compiler;
+using System.Security.Cryptography.X509Certificates;
+using System.Reflection;
+using System .ComponentModel.DataAnnotations;
+namespace MvcMovie.Models;
+
+public class MvcMovie
+{
+    public int id { get;set;}
+
+public string? title { get; set; }
+[DataType(DataType.Date)]
+
+public DateTime releaseDate { get; set; }
+
+Public string? Genre { get; set; }
+
+public decimal price { get; set; }
+
+}
+
+
--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a627193cfe6a0a4eae2e36877b1d2de9668c8435")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0805a0ec67f05a8b1278dbff0c9f6447d52a0477")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
