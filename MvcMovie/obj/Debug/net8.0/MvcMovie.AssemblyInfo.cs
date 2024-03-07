27fcaa8cb233a54b1d1ef20d792501e283f108dc
tranducoan <sonsuke655@gmail.com>
2024-03-07 16:30:00 +0700

    7-MVC SLIDE 17

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+befd53a177ba3b4d4fd87e69b26c9f36704be2bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32b7aa6d4e98eebfbdf9edee693a5f005276b29c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
