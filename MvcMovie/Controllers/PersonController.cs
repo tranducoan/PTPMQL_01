d45a86c2e2b77a1e76e3dab09fbd849d089aa717
tranductoan <100502387+tranducoan@users.noreply.github.com>
2024-03-07 17:07:34 +0700

    Update PersonController.cs

---
+++ MvcMovie/Controllers/PersonController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+
+namespace DemoMVC.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Person()); 
+        }
+
+        [HttpPost]
+        public IActionResult Index(Person person)
+        {
+           ViewBag.thongBao = "Xin chao " + person.PersonId + " - " + person.FullName + " - " + person.Address ;
+            return View(person); 
+        }
+    }
+}
+// Tran Đuc toan-2021050642
+
