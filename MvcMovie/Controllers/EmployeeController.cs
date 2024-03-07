a627193cfe6a0a4eae2e36877b1d2de9668c8435
tranducoan <sonsuke655@gmail.com>
2024-03-07 16:37:27 +0700

    7-MVC T17

---
+++ MvcMovie/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using DemoMVC.Models;
+
+namespace DemoMVC.Controllers
+{
+//Tran Đuc toan  - 2021050642
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Employee()); 
+        }
+
+        [HttpPost]
+        public IActionResult Index(Employee employee)
+        {
+            
+            ViewBag.Message = "Thông tin nhân viên: " + employee.EmployeeId + " tuoi " + employee.Age;
+            return View(employee);
+        }
+    }
+}
--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27fcaa8cb233a54b1d1ef20d792501e283f108dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cd016361ec42c31daa98ffbb3c464227d9e906a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
