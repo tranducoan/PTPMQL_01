104f96f63982b2ab465b42be851eb6005b5b8cef
tranductoan <100502387+tranducoan@users.noreply.github.com>
2024-04-21 19:42:15 +0700

    bài thực hành biến số chương 5 C# trang số 15

---
+++ Net_Core/NewApp/bienso.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int maSinhvien = 2021050642;
+        Console.WriteLine("Mã của sinh viên là : " + maSinhvien);
+    }
+}
+
+  
+
