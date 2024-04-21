483e3052f436dc8fc3fef6a159e56ef42f7e86f6
tranductoan <100502387+tranducoan@users.noreply.github.com>
2024-04-21 19:26:03 +0700

    cập nhật code chương 5 c#

--- Net_Core/NewApp/Program.cs
+++ Net_Core/NewApp/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+//in ra màn hình: 6
+Console.WriteLine(6);
+Console.WriteLine(9);
+
+ int a = 9; //khai bao bien so a kieu int va gan gia tri = 9
+ Console.WriteLine("a = " + a);
+
+//in ra man hinh: a + b = 12
+ int a1 = 9;
+ int b1 = 3;
+ Console.WriteLine("{0} + {1} = {2}", a1, b1, a1+b1);
