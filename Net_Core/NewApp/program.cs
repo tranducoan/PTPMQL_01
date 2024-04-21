45837373d7cf0c10e75661bb103fb9da92e833ec
tranductoan <100502387+tranducoan@users.noreply.github.com>
2024-04-21 21:15:54 +0700

    Add files via upload

--- Net_Core/NewApp/program.cs
+++ Net_Core/NewApp/program.cs
@@ -1,20 +1,18 @@
+using System.Dynamic;
 namespace NewApp;
 
 using System;
 
 internal class Program
 {
-    // ví dụ về chuyển đổi dữ liệu chương 5 C# trang 29
-    private static void Main()
+    // ví dụ về chuyển đổi dữ liệu chương 5 C# trang 29 sử dung lớp Convert
+    private static void Main(string[] args)
     {
         //giá trị đầu vào của chuyển đổi 
 string str ="123";
-//biến lưu kết quả sau khi chuyển đổi 
-int ketqua;
-//kiểm tra chuyển đổi có thành công không 
-bool kiemtra = false;
-//chuyển đổi kiểu dư liệu 
-kiemtra = int.TryParse(str,out(ketqua));
-System.Console.WriteLine("ket qua" +ketqua);
+//chuyển đổi kiểu dữ liệu string giá trị ="123"
+//sang kiểu dữ liệu int giá trị sau khi chuyển đổi = 123
+int a = Convert.ToInt32(str);
+System.Console.WriteLine("a" + a);
     }
 }
