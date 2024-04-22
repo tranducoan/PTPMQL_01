d8de113725e6b2fe2d114e3dc832c876d63c30a1
tranductoan <100502387+tranducoan@users.noreply.github.com>
2024-04-22 08:40:45 +0700

    bài thực hành chương 5 C# toán tử trang 33

--- Net_Core/NewApp/Program.cs
+++ Net_Core/NewApp/Program.cs
@@ -1,15 +1,25 @@
-using System;
-
-class Program
+//nhập 2 số từ bàn phím và thực hiện các yêu cầu sau:
+//tính hiệu
+internal class Program
 {
-    static void Main()
+    private static void Main(string[] args)
     {
-// khai báo biến kiểu string và gán giá trị là trần đức toàn 
-        // khai báo biến kiểu int và gán giá trị là 4
-        const string hoTen = "trần đức toàn ";
-        const int namLamViec = 4;
-
-        Console.WriteLine("Họ và tên: " + hoTen);
-        Console.WriteLine("Năm làm việc: " + namLamViec);
+       int a;//khai bao bien a kieu int
+       int b;//khai báo bien b kieu int
+       //thong bao cho người dùng nhập vào a
+       System.Console.Write("a = ");
+       //doc du lieu nhap vao tu ban phim chuyen sang kieu int va gan cho a
+       a = Convert.ToInt32(Console.ReadLine());
+       System.Console.Write("b = ");
+       b = Convert.ToInt32(Console.ReadLine());
+       //in hieu a-b ra ngoai man hinh
+       System.Console.WriteLine("{0} - {1} = {2}",a,b,a-b);
+//tính tổng của 2 số và in ra màn hình 
+         System.Console.WriteLine("{0} + {1} = {2}",a,b,a+b);
+//tinh tich 2 so va in ra man hinh
+        System.Console.WriteLine("{0} * {1} = {2}",a,b,a*b);
+//thuc hien phep chia lay phan nguyen va lay du 2 so => in ra man hinh//
+        System.Console.WriteLine("{0} / {1} = {2}",a,b,a/b);
+        System.Console.WriteLine("{0} % {1} = {2}", a, b, a%b);
     }
 }
