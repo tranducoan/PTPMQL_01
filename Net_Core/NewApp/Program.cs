d2bf6b813ac572e6a63c33886f0446c027e352d2
tranductoan <100502387+tranducoan@users.noreply.github.com>
2024-04-22 08:56:32 +0700

    bài thực hành chương 5 C# toán tử so sánh trang 35

--- Net_Core/NewApp/Program.cs
+++ Net_Core/NewApp/Program.cs
@@ -1,25 +1,27 @@
-//nhập 2 số từ bàn phím và thực hiện các yêu cầu sau:
-//tính hiệu
-internal class Program
+using System;
+
+namespace NewApp
 {
-    private static void Main(string[] args)
+    public class Program
     {
-       int a;//khai bao bien a kieu int
-       int b;//khai báo bien b kieu int
-       //thong bao cho người dùng nhập vào a
-       System.Console.Write("a = ");
-       //doc du lieu nhap vao tu ban phim chuyen sang kieu int va gan cho a
-       a = Convert.ToInt32(Console.ReadLine());
-       System.Console.Write("b = ");
-       b = Convert.ToInt32(Console.ReadLine());
-       //in hieu a-b ra ngoai man hinh
-       System.Console.WriteLine("{0} - {1} = {2}",a,b,a-b);
-//tính tổng của 2 số và in ra màn hình 
-         System.Console.WriteLine("{0} + {1} = {2}",a,b,a+b);
-//tinh tich 2 so va in ra man hinh
-        System.Console.WriteLine("{0} * {1} = {2}",a,b,a*b);
-//thuc hien phep chia lay phan nguyen va lay du 2 so => in ra man hinh//
-        System.Console.WriteLine("{0} / {1} = {2}",a,b,a/b);
-        System.Console.WriteLine("{0} % {1} = {2}", a, b, a%b);
+        // đề bài : Toán tử so sánh:
+//Thực hành: Nhập vào 2 số từ bàn phím, tiến hành so sánh 2 số vào in kết quả màn hình. 
+        private static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+
+            Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
+            Console.WriteLine("{0} % {1} = {2}", a, b, a % b);
+
+            if (a < b)
+            {
+                Console.WriteLine("{0} nhỏ hơn {1}", a, b);
+            }
+            else
+            {
+                Console.WriteLine("{0} lớn hơn {1}", a, b);
+            }
+        }
     }
 }
