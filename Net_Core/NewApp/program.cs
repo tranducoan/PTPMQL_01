270bbc459ef7be3d23b243e4413999ba16a2852e
tranductoan <100502387+tranducoan@users.noreply.github.com>
2024-04-22 11:10:41 +0700

    ví dụ về vòng lặp For chương 5 trang 51

--- Net_Core/NewApp/program.cs
+++ Net_Core/NewApp/program.cs
@@ -1,20 +1,10 @@
-public class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
-           int day =5;
-           switch(day)
-           {
-            case 1 : System.Console.WriteLine("thu 1"); break;
-      case 2 : System.Console.WriteLine("Thu 2"); break;
-      case 3 : System.Console.WriteLine("Thu 3"); break;
-      case 4 : System.Console.WriteLine("Thu 4"); break;
-      case 5 : System.Console.WriteLine("Thu 5"); break;
-      case 6 : System.Console.WriteLine("Thu 6"); break;
-      case 7 : System.Console.WriteLine("Thu 7"); break;
-      default :
-        System.Console.WriteLine("Khong phai ngay trong tuan");
-        break;
-           }
+          for ( int i= 0; i<20;i++)
+          {
+            System.Console.WriteLine("vòng lặp thứ {0} ",i);
+          }
         }
-    }
+   
