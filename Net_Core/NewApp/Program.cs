ffb85032daffc13bbed1d00d394214570f1b2aa1
tranductoan <100502387+tranducoan@users.noreply.github.com>
2024-04-23 10:44:20 +0700

    ví dụ vòng lặp do... while chương 5 trang 57

--- Net_Core/NewApp/Program.cs
+++ Net_Core/NewApp/Program.cs
@@ -1,14 +1,14 @@
-public class Program
+ public class Program
     {
         public static void Main(string[] args)
         {
             int i= 1;
-          while (i < 30)
+          do
           {
             System.Console.WriteLine("vòng lặp thứ {0} ",i);
             //tăng i lên 1 đơn vị để đảm bảo rằng có lúc i>=10
             //i>=10 thì vòng lặp kết thúc 
             i++;
-          }
+          } while(i<10);
         }
     }
