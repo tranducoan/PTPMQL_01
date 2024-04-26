1b3df7def4ed405fa370994ae9ad564839c30a69
tranductoan <100502387+tranducoan@users.noreply.github.com>
2024-04-26 16:43:06 +0700

    class fruit

--- Net_Core/NewApp/Person.cs
+++ Net_Core/NewApp/Person.cs
@@ -1,22 +1,23 @@
 namespace NewApp.Models
 {
-    public class Student
+    public class Fruit
     {
-        public int MaSinhVien { get; set; }
-        public string TenSinhVien { get; set; }
-        public string Nganh { get; set; }
+        public string TenQua { get; set; }
+        public int GiaBan { get; set; }
+        public int KhoiLuong { get; set; }
         public void NhapThongTin()
         {
-            System.Console.Write("Ma Sinh Vien = ");
-            MaSinhVien = Convert.ToInt32(Console.ReadLine());
-            System.Console.Write("Ten Sinh Vien = ");
-            TenSinhVien = Console.ReadLine();
-            System.Console.Write("Nganh = ");
-            Nganh = Console.ReadLine();
+            
+           System.Console.Write("Ten Qua = ");
+           TenQua = Console.ReadLine();
+           System.Console.Write("GiaBan = ");
+           GiaBan = Convert.ToInt32(Console.ReadLine());
+           System.Console.Write("Khoi Luong = ");
+           KhoiLuong = Convert.ToInt32(Console.ReadLine());
         }
         public void HienThi()
         {
-            System.Console.WriteLine("{0} - {1} - {2} ",MaSinhVien, TenSinhVien, Nganh);
+            System.Console.WriteLine("{0} - {1} - {2} kg", TenQua, GiaBan, KhoiLuong);
         }
     }
 }
