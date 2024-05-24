1b2b5ddd1773d2100a9badbfdb15dc3bd70cd6fb
tranductoan <100502387+tranducoan@users.noreply.github.com>
2024-05-24 14:23:52 +0700

    Cập nhật mã nguồn của PersonController chương 7 trang 97 Phần Phân Trang

--- MvcMovie/Controllers/PersonController.cs
+++ MvcMovie/Controllers/PersonController.cs
@@ -19,11 +19,22 @@ namespace MvcMovie.Controllers
         {
             _context = context;
         }
-      public async Task<IActionResult> Index(int? page)
-        {
-            var model =  _context.Person.ToList().ToPagedList(page?? 1,5 );
-            return View(model);
-        }
+     public async Task<IActionResult> Index(int? page, int? PageSize)
+{
+    ViewBag.PageSize = new List<SelectListItem>()
+    {
+        new SelectListItem() {Value="3", Text= "3"},
+        new SelectListItem() {Value="5", Text= "5"},
+        new SelectListItem() {Value="10", Text= "10"},
+        new SelectListItem() {Value="15", Text= "15"},
+        new SelectListItem() {Value="25", Text= "25"},
+        new SelectListItem() {Value="50", Text= "50"},
+    };
+    int pagesize = (PageSize ?? 3);
+    ViewBag.psize = pagesize;
+    var model = _context.Person.ToList().ToPagedList(page ?? 1, pagesize);
+    return View(model);
+}
         public IActionResult Create()
         {
             return View();
